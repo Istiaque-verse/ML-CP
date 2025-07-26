2ed469b7d59a2ed9c0705007f7074e12dee21c39
GreenCycle Developer <user@example.com>
2025-07-26 16:13:56 +0600

    Added database connection

--- src/main/java/com/mlcp/mlcp/MlCpApplication.java
+++ src/main/java/com/mlcp/mlcp/MlCpApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MlCpApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MlCpApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MlCpApplication.class, args);
+    }
 }
---
+++ src/main/java/com/mlcp/mlcp/Problem.java
@@ -0,0 +1,21 @@
+package com.mlcp.mlcp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+
+@Entity
+@Table(name = "problems")
+public class Problem {
+    @Id
+    private Long id;
+
+    private String title;
+    private String description;
+    private String link;
+    private String topics;
+
+    // Getters and Setters
+}
